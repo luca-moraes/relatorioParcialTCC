47457ba93c8c53abda238a97263af748a1a52d61
luca-moraes <lcansd.lm@gmail.com>
2024-05-23 05:08:06 -0300

    Testes de percentual de erro do algoritmo

--- solutionCode/PercentualTests.py
+++ solutionCode/PercentualTests.py
@@ -6,6 +6,19 @@ from Models import Question, Answer, RefResponse, Keywords, AnswerParams, Answer
 def write_to_json(data, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+        
+def calculate_general_error(answer_tests):
+    total_error = 0
+    num_answers = len(answer_tests)
+
+    for answer_test in answer_tests:
+        total_error += answer_test.percentual_error
+
+    general_error = total_error / num_answers
+    
+    print("Numero de Testes: {}".format(num_answers))
+    print("Erro Geral do Algoritmo: {:.2%}".format(general_error))
+
 
 def test_all(peso1, peso2, peso3, answers):
     answerTestsList = []
@@ -21,22 +34,24 @@ def test_all(peso1, peso2, peso3, answers):
 
         nota = soma / peso
 
-        percentual = 0
+        error = 0
         if(answer.answer_values.grade == 0):
-            percentual = 
+            error = nota / 3
         else:
-            percentual = nota / answer.answer_values.grade
-
-        error = 1 - percentual
+            # percentual = nota / answer.answer_values.grade
+            percentual = (min(nota, answer.answer_values.grade) / max(nota, answer.answer_values.grade))
+            error = 1 - percentual
 
         answerTestsList.append(AnswerTestParams(answer.answer_number,
-                answer.answer_values,
-                answer.frequence_similarity,
-                answer.liv_distance,
-                answer.bert_score,
-                nota,
-                error
-                ))
+            answer.answer_values,
+            answer.frequence_similarity,
+            answer.liv_distance,
+            answer.bert_score,
+            nota,
+            error
+        ))
+    
+    calculate_general_error(answerTestsList)
     
     answerTestsDict = [asdict(answerTests) for answerTests in answerTestsList]
     write_to_json(answerTestsDict, '../normalizedData/ptbrDataset/testsResults.json')
